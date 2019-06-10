3858779ed6ffeb9350a1a925a57c4e8d415de622
Danilo Paggi <danilopaggi@gmail.com>
2019-06-10 13:05:37 -0300

    A-Rotas com token

--- API/start/routes.js
+++ API/start/routes.js
@@ -3,3 +3,8 @@
 const Route = use('Route')
 
 Route.post('/users', 'UserController.store').validator('User')
+
+// Tratamento de rotas com autenticação
+/* Route.group(() => {
+
+}).middleware(['auth']) */
