8cf7dabf5dbee62b5b85fa7dbf3bd135127e3bfa
Danilo Paggi <danilopaggi@gmail.com>
2019-06-10 12:37:10 -0300

    A-Validators. internacionalização e youch

--- API/app/Validators/User.js
+++ API/app/Validators/User.js
@@ -1,12 +1,19 @@
 'use strict'
+const Antl = use('Antl')
 
 class User {
+  get validateAll () {
+    return true
+  }
   get rules () {
     return {
       name: 'required',
       email: 'required|email|unique:users',
       password: 'required|confirmed'
     }
+  }
+  get messages () {
+    return Antl.list('validation')
   }
 }
 
