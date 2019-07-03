02fd4db2d7116dc7bd2ab9e0e678aa34f1c400c6
Danilo Paggi <danilopaggi@gmail.com>
2019-07-03 11:17:16 -0300

    M- Nova inclusão do degrade

--- Mobile/src/pages/SignIn/index.js
+++ Mobile/src/pages/SignIn/index.js
@@ -1,31 +1,37 @@
 import React from 'react';
-import { Text, View, StatusBar, Image, TouchableOpacity } from 'react-native';
+import {
+  Text,
+  View,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import PropTypes from 'prop-types';
-//import LinearGradient from 'react-native-linear-gradient';
-import styles from './styles';
+import LinearGradient from 'react-native-linear-gradient';
+import style from './styles';
 import { colors } from '~/styles';
-import LogIn from '~/components/LogIn'
+import LogIn from '~/components/LogIn';
 
 const SignIn = ({ navigation }) => (
-  <View style={styles.container}>
+  <View style={style.container}>
     <StatusBar backgroundColor={colors.black} barStyle="light-content" />
-    <Image  style={styles.background} source={require('~/assets/img/background.png')}/>
-    {/*<LinearGradient style={styles.background} colors={[colors.transparent, colors.black]} locations={[0,0.97]}/>*/}
-    <View style={styles.containerForm}>
-      <View style={styles.form}>
-        <LogIn sucessLogIn={()=>{navigation.navigate('Products')}}/>
-        <TouchableOpacity style={[styles.buttonSignUp]} onPress={()=>{navigation.navigate('SignUp')}}>
-          <Text style={styles.buttonText}>Criar conta gratuita</Text>
+    <Image style={style.background} source={require('~/assets/img/background.png')} />
+    <LinearGradient style={style.background} colors={[colors.transparent, colors.black]} locations={[0, 0.97]} />
+    <View style={style.containerForm}>
+      <View style={style.form}>
+        <LogIn sucessLogIn={() => { navigation.navigate('Products'); }} />
+        <TouchableOpacity style={[style.buttonSignUp]} onPress={() => { navigation.navigate('SignUp'); }}>
+          <Text style={style.buttonText}>Criar conta gratuita</Text>
         </TouchableOpacity>
       </View>
     </View>
   </View>
 );
 
-SignIn.propTypes={
+SignIn.propTypes = {
   navigation: PropTypes.shape({
     navigate: PropTypes.func,
   }).isRequired,
-}
+};
 
 export default SignIn;
